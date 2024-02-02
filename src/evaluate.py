cb7649e2cbc09b0328222f1cf06b8f1a947b1559
cliedl <christian.liedl@gmx.net>
2024-02-02 16:13:10 +0100

    Added f1 score for training data

--- src/evaluate.py
+++ src/evaluate.py
@@ -3,5 +3,9 @@ from sklearn.metrics import f1_score
 def evaluate_model(pipe, X_train, X_valid, y_train, y_valid):
     pipe.fit(X_train, y_train)
     preds_valid = pipe.predict(X_valid)
-    score = f1_score(y_valid, preds_valid, average='micro')
-    return score
+    preds_train = pipe.predict(X_train)
+
+    score_valid = f1_score(y_valid, preds_valid, average='micro')
+    score_train = f1_score(y_valid, preds_valid, average='micro')
+    
+    return score_valid, score_train
