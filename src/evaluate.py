4fab91ad2b59a7ff5cca0cd0a79f32610ea3f1cb
cliedl <christian.liedl@gmx.net>
2024-02-02 17:06:23 +0100

    Fixed bug: now evaluate_model returns correct train f1 score

--- src/evaluate.py
+++ src/evaluate.py
@@ -20,6 +20,6 @@ def evaluate_model(pipe, X_train, X_valid, y_train, y_valid):
     preds_train = pipe.predict(X_train)
 
     score_valid = f1_score(y_valid, preds_valid, average='micro')
-    score_train = f1_score(y_valid, preds_valid, average='micro')
-    
+    score_train = f1_score(y_train, preds_train, average='micro')
+
     return score_valid, score_train
