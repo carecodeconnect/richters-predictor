d9e64072a6bc74d8e7e7bac954f1ff87013399c1
cliedl <christian.liedl@gmx.net>
2024-02-03 08:56:05 +0100

    added train_on_all attribute in evaluate() to enable training on validation data

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,6 +1,6 @@
 from sklearn.metrics import f1_score
 
-def evaluate_model(pipe, X_train, X_valid, y_train, y_valid):
+def evaluate_model(pipe, X_train, X_valid, y_train, y_valid, train_on_all=False):
     """
     Evaluate the model using the F1 score.
 
@@ -10,12 +10,18 @@ def evaluate_model(pipe, X_train, X_valid, y_train, y_valid):
         X_valid: validation data
         y_train: training labels
         y_valid: validation labels
+        train_on_all: If True, train also on (test AND validation data)
 
     Returns:
         score_valid: F1 score on validation data
         score_train: F1 score on training data
     """
-    pipe.fit(X_train, y_train)
+
+    if train_on_all:
+        pipe.fit(pd.concat([X_train, X_valid]), pd.concat([y_train, y_valid]))
+    else:
+        pipe.fit(X_train, y_train)
+
     preds_valid = pipe.predict(X_valid)
     preds_train = pipe.predict(X_train)
 
